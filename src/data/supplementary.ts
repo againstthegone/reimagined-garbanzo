fe9b0fa1d6961aca23778a4524c16b5861e26f92
Robert Chow <rchow@thoughtworks.com>
2020-03-31 19:33:22 +0100

    Added supplementary data not available to COVID-19 set

---
+++ src/data/supplementary.ts
@@ -0,0 +1,90 @@
+export default {
+    data: [
+        {
+            country: 'China',
+            timeline: [
+                {
+                    date: '31-12-2019',
+                    confirmed: 27
+                },
+                {
+                    date: '10-01-2020',
+                    confirmed: 41
+                },
+                {
+                    date: '16-01-2020',
+                    confirmed: 45
+                },
+                {
+                    date: '17-01-2020',
+                    confirmed: 62
+                },
+                {
+                    date: '18-01-2020',
+                    confirmed: 121
+                },
+                {
+                    date: '19-01-2020',
+                    confirmed: 198
+                },
+                {
+                    date: '20-01-2020',
+                    confirmed: 291
+                },
+                {
+                    date: '21-01-2020',
+                    confirmed: 440
+                }
+            ]
+        },
+        {
+            country: 'Japan',
+            timeline: [
+                {
+                    date: '16-01-2020',
+                    confirmed: 1
+                }
+            ]
+        },
+        {
+            country: 'Korea, South',
+            timeline: [
+                {
+                    date: '20-01-2020',
+                    confirmed: 1
+                }
+            ]
+        },
+        {
+            country: 'Taiwan',
+            timeline: [
+                {
+                    date: '20-01-2020',
+                    confirmed: 1
+                }
+            ]
+        },
+        {
+            country: 'Thailand',
+            timeline: [
+                {
+                    date: '13-01-2020',
+                    confirmed: 1
+                },
+                {
+                    date: '17-01-2020',
+                    confirmed: 2
+                }
+            ]
+        },
+        {
+            country: 'US',
+            timeline: [
+                {
+                    date: '15-01-2020',
+                    confirmed: 1
+                }
+            ]
+        },
+    ]
+};
