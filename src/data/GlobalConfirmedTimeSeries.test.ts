49bb632752be91aef06032716665a7709cb16321
Robert Chow <rchow@thoughtworks.com>
2020-04-04 21:28:02 +0100

    Parse global confirmed data into country case datas

--- src/data/GlobalConfirmedTimeSeries.test.ts
+++ src/data/GlobalConfirmedTimeSeries.test.ts
@@ -10,8 +10,8 @@ describe('Global Confirmed Time Series', () => {
             fs.unlinkSync(csvFileName);
         });
 
-        it('is able to transcribe a simple csv data string to a series of country case entries', () => {
-            const csvData = 'Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,1/24/20\r\nHubei,China,30.9756,112.2707,444,444,549';
+        it('is able to transcribe csv data of a single entry to country case entries', () => {
+            const csvData = 'Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,1/24/20\r\nHubei,China,30.9756,112.2707,444,444,549\r\n';
             fs.writeFileSync(csvFileName, csvData);
 
             const caseEntries = fromCsvFile(csvFileName);
@@ -35,13 +35,122 @@ describe('Global Confirmed Time Series', () => {
                 }
             ]);
         });
+
+        it('is able to transcribe csv data of multiple entries to country case entries', () => {
+            const csvData = 'Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,1/24/20\r\n' +
+                'Hubei,China,30.9756,112.2707,444,444,549\r\n' + 
+                ',Japan,36.0,138.0,2,2,2\r\n' + 
+                ',"Korea, South",36.0,128.0,1,1,2\r\n';
+            fs.writeFileSync(csvFileName, csvData);
+
+            const caseEntries = fromCsvFile(csvFileName);
+            expect(caseEntries).toEqual([
+                {
+                    country: 'China',
+                    timeline: [
+                        {
+                            date: '2020-01-22',
+                            confirmed: 444
+                        },
+                        {
+                            date: '2020-01-23',
+                            confirmed: 444,
+                        },
+                        {
+                            date: '2020-01-24',
+                            confirmed: 549,
+                        }
+                    ]
+                },
+                {
+                    country: 'Japan',
+                    timeline: [
+                        {
+                            date: '2020-01-22',
+                            confirmed: 2
+                        },
+                        {
+                            date: '2020-01-23',
+                            confirmed: 2
+                        },
+                        {
+                            date: '2020-01-24',
+                            confirmed: 2
+                        }
+                    ]
+                },
+                {
+                    country: 'Korea, South',
+                    timeline: [
+                        {
+                            date: '2020-01-22',
+                            confirmed: 1
+                        },
+                        {
+                            date: '2020-01-23',
+                            confirmed: 1
+                        },
+                        {
+                            date: '2020-01-24',
+                            confirmed: 2
+                        }
+                    ]
+                }
+            ]);
+        });
+
+        it('is able to transcribe csv data of multiple entries of the same country to country case entries', () => {
+            const csvData = 'Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,1/24/20\r\n' +
+                'Hubei,China,30.9756,112.2707,444,444,549\r\n' + 
+                'Shanghai,China,31.201999999999998,121.4491,9,16,20\r\n' +
+                ',Japan,36.0,138.0,2,2,2\r\n';
+            fs.writeFileSync(csvFileName, csvData);
+
+            const caseEntries = fromCsvFile(csvFileName);
+            expect(caseEntries).toEqual([
+                {
+                    country: 'China',
+                    timeline: [
+                        {
+                            date: '2020-01-22',
+                            confirmed: 453
+                        },
+                        {
+                            date: '2020-01-23',
+                            confirmed: 460
+                        },
+                        {
+                            date: '2020-01-24',
+                            confirmed: 569,
+                        }
+                    ]
+                },
+                {
+                    country: 'Japan',
+                    timeline: [
+                        {
+                            date: '2020-01-22',
+                            confirmed: 2
+                        },
+                        {
+                            date: '2020-01-23',
+                            confirmed: 2
+                        },
+                        {
+                            date: '2020-01-24',
+                            confirmed: 2
+                        }
+                    ]
+                }
+            ]);
+        });
     });
 
     describe('Integration Test', () => {
         it('is able to read the country case data from the real data', () => {
             const caseEntries = fromCsvFile();
             expect(caseEntries).toBeTruthy();
-            expect(caseEntries.find(({ country }) => country === 'China').timeline[0].confirmed).toBeGreaterThan(0);
+            expect(caseEntries.find(({ country }) => country === 'China').timeline[0].confirmed).toEqual(548);
         });
     });
 });
--- src/data/GlobalConfirmedTimeSeries.ts
+++ src/data/GlobalConfirmedTimeSeries.ts
@@ -14,20 +14,32 @@ export function fromCsvFile(csvPath = 'COVID-19/csse_covid_19_data/csse_covid_19
         console.log(errors);
         throw new Error(`Unable to parse Global Confirmed Time Series from ${csvPath}:\n\t${errors}`);
     }
-    return data.map(province => {
-        const country = province[COUNTRY_HEADER];
-        const timeline = Object.keys(province)
-            .filter(key => key.match(/^\d+\/\d+\/\d+$/))
-            .map(key => {
-                const [monthCapture, dayCapture, yearCapture] = key.match(/\d+/g);
-                const isoDay = dayCapture.padStart(2, '0');
-                const isoMonth = monthCapture.padStart(2, '0');
-                const isoYear = yearCapture.padStart(4, '20');
-                return { date: `${isoYear}-${isoMonth}-${isoDay}`, confirmed: parseInt(province[key]) };
-            });
-        return {
-            country,
-            timeline,
-        };
-    });
+    return data
+        .map(province => {
+            const country = province[COUNTRY_HEADER];
+            const timeline = Object.keys(province)
+                .filter(key => key.match(/^\d+\/\d+\/\d+$/))
+                .map(key => {
+                    const [monthCapture, dayCapture, yearCapture] = key.match(/\d+/g);
+                    const isoDay = dayCapture.padStart(2, '0');
+                    const isoMonth = monthCapture.padStart(2, '0');
+                    const isoYear = yearCapture.padStart(4, '20');
+                    return { date: `${isoYear}-${isoMonth}-${isoDay}`, confirmed: parseInt(province[key]) };
+                })
+            return {
+                country,
+                timeline,
+            } as CountryCaseData;
+        })
+        .reduce((countryCaseDatas, provinceCaseData) => {
+            const countryCaseData = countryCaseDatas.find(({ country }) => country === provinceCaseData.country);
+            if (countryCaseData) {
+                provinceCaseData.timeline
+                    .forEach(({ date, confirmed }) =>
+                        countryCaseData.timeline.find(({ date: countryDate }) => countryDate === date).confirmed += confirmed)
+            } else {
+                countryCaseDatas.push(provinceCaseData);
+            }
+            return countryCaseDatas;
+        }, [] as CountryCaseData[]);
 }
