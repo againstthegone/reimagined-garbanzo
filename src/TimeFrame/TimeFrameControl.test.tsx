13ae409e763921799b0ebb1ff685448255baf4b9
Robert Chow <rchow@thoughtworks.com>
2020-08-27 15:45:32 +0100

    Add date to time frame control

--- src/TimeFrame/TimeFrameControl.test.tsx
+++ src/TimeFrame/TimeFrameControl.test.tsx
@@ -2,18 +2,21 @@ import React, { Dispatch } from 'react';
 import { fireEvent, render, screen } from "@testing-library/react";
 import { TimeFrameControl } from './TimeFrameControl';
 import { TimeFrameChangedEvent } from './TimeFrameChangedEvent';
-import { useCovid19Dispatch } from '../Covid19/Covid19Context';
+import { useCovid19Dispatch, useCovid19State } from '../Covid19/Covid19Context';
 import { Covid19Event } from '../Covid19/Covid19Event';
+import { Covid19State } from '../Covid19/Covid19State';
 
 jest.mock('../Covid19/Covid19Context');
 
 describe('TimeFrameControl', () => {
 
     const mockCovid19Dispatch = jest.fn();
+    const mockCovid19State = { timeFrame: 0 } as Covid19State;
 
     beforeEach(() => {
         jest.resetAllMocks();
         (useCovid19Dispatch as jest.Mock<Dispatch<Covid19Event>>).mockReturnValue(mockCovid19Dispatch);
+        (useCovid19State as jest.Mock<Covid19State>).mockReturnValue(mockCovid19State);
     });
 
     it('renders an input range', () => {
@@ -22,10 +25,47 @@ describe('TimeFrameControl', () => {
         expect(input.getAttribute('type')).toEqual('range');
     });
 
+    it('renders an input with a value matching that of the time frame', () => {
+        const timeFrame = 51;
+        (useCovid19State as jest.Mock<Covid19State>).mockReturnValue({ timeFrame } as Covid19State);
+
+        render(<TimeFrameControl/>);
+        const input = screen.getByLabelText('input-range');
+        expect(input.getAttribute('value')).toEqual(`${timeFrame}`);
+    });
+
+    it('renders an input with a min of 0', () => {
+        render(<TimeFrameControl/>);
+        const input = screen.getByLabelText('input-range');
+        expect(input.getAttribute('min')).toEqual('0');
+    });
+
+    it('renders an input with a max matching that of the number of days since the 2020-03-01', () => {
+        const dateSpy = jest.spyOn(global.Date, 'now').mockImplementation(() => Date.UTC(2020, 4, 30, 12));
+        render(<TimeFrameControl/>);
+        const input = screen.getByLabelText('input-range');
+        expect(input.getAttribute('max')).toEqual('90');
+        dateSpy.mockRestore();
+    });
+
     it('triggers a time frame changed event on input changed', () => {
         render(<TimeFrameControl />);
         const input = screen.getByLabelText('input-range');
         fireEvent.input(input, { target: { value: 3 }});
         expect(mockCovid19Dispatch).toHaveBeenCalledWith(new TimeFrameChangedEvent(3));
-    })
+    });
+
+    it('initially renders the base date 2020-03-01', () => {
+        render(<TimeFrameControl/>);
+        const dateText = screen.getByText('2020-03-01');
+        expect(dateText).toBeInTheDocument();
+    });
+
+    it('renders a date that reflects the time frame relative to the base date', () => {
+        (useCovid19State as jest.Mock<Covid19State>).mockReturnValue({ timeFrame: 40 } as Covid19State);
+
+        render(<TimeFrameControl/>);
+        const dateText = screen.getByText('2020-04-10');
+        expect(dateText).toBeInTheDocument();
+    });
 });
--- src/TimeFrame/TimeFrameControl.tsx
+++ src/TimeFrame/TimeFrameControl.tsx
@@ -1,15 +1,34 @@
 import React, { ChangeEvent } from 'react';
-import { useCovid19Dispatch } from '../Covid19/Covid19Context';
+import { useCovid19Dispatch, useCovid19State } from '../Covid19/Covid19Context';
 import { TimeFrameChangedEvent } from './TimeFrameChangedEvent';
+
+const MILLIS_PER_SECOND = 1000;
+const SECONDS_PER_MINUTE = 60;
+const MINUTES_PER_HOUR = 60;
+const HOURS_PER_DAY = 24;
+
+const BASE_DATE_TIME = Date.UTC(2020, 2, 1);
+
+const MILLIS_PER_DAY = MILLIS_PER_SECOND * SECONDS_PER_MINUTE * MINUTES_PER_HOUR * HOURS_PER_DAY;
+
+const useCovid19TimeFrame = () => {
+    const state = useCovid19State();
+    return state.timeFrame;
+};
 
 export const TimeFrameControl = () => {
 
     const dispatch = useCovid19Dispatch();
+    const timeFrame = useCovid19TimeFrame();
+    const daysSinceBaseDate = Math.floor((Date.now() - BASE_DATE_TIME) / MILLIS_PER_DAY);
 
     const onChange = (e: ChangeEvent<HTMLInputElement>) => {
         e.preventDefault();
         dispatch(new TimeFrameChangedEvent(parseInt(e.target.value)));
     };
 
-    return (<input type='range' aria-label='input-range' onChange={onChange} className='time-frame-control' />);
+    return (<div className='time-frame-control'>
+        <div>{new Date(BASE_DATE_TIME + (timeFrame * MILLIS_PER_DAY)).toISOString().substring(0, 10)}</div>
+        <input type='range' aria-label='input-range' onChange={onChange} className='time-frame-control' value={timeFrame} min='0' max={daysSinceBaseDate} />
+    </div>);
 }
