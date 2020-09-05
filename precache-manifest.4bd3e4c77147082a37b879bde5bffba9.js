c7af61990ffd061e4a43963c8bde720eeb5199c4
Robert Chow <rchow@thoughtworks.com>
2020-09-05 09:54:32 +0100

    Updates

---
+++ precache-manifest.4bd3e4c77147082a37b879bde5bffba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c660474fb5d860c706931b5a1d22a8",
+    "url": "/reimagined-garbanzo/index.html"
+  },
+  {
+    "revision": "18e78f99a7064c938d91",
+    "url": "/reimagined-garbanzo/static/css/main.db9dfe32.chunk.css"
+  },
+  {
+    "revision": "b6702e9c262575b36fa0",
+    "url": "/reimagined-garbanzo/static/js/2.2e1e2b69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reimagined-garbanzo/static/js/2.2e1e2b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e78f99a7064c938d91",
+    "url": "/reimagined-garbanzo/static/js/main.16a63ebe.chunk.js"
+  },
+  {
+    "revision": "4504e2135f23cbf84d03",
+    "url": "/reimagined-garbanzo/static/js/runtime-main.5c73478c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/reimagined-garbanzo/precache-manifest.4bd3e4c77147082a37b879bde5bffba9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reimagined-garbanzo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
