4e755f770092c151f6216ec7cf9e6e6523cc0ed6
Robert Chow <rchow@thoughtworks.com>
2020-09-06 09:33:35 +0100

    Amend colors to red, show datum and align data with correct date

--- src/DataNode/NewCasesBySpecimenDateDataNode.tsx
+++ src/DataNode/NewCasesBySpecimenDateDataNode.tsx
@@ -4,6 +4,7 @@ import { LowerTierLocalAuthorityAreaName } from '../CoronovirusDataGovUkApi/Lowe
 import { AreaDataStatus } from '../AreaData/AreaDataStatus';
 import { useTimeFrame } from '../TimeFrame/useTimeFrame';
 import { useCovid19AreaData } from '../AreaData/Covid19AreaData';
+import { BASE_DATE_TIME, MILLIS_PER_DAY } from '../TimeFrame/TimeFrameControl';
 
 interface NewCasesBySpecimenDateDataNodeProps {
     area: LowerTierLocalAuthorityAreaName;
@@ -13,17 +14,23 @@ export const NewCasesBySpecimenDateDataNode = ({ area }: NewCasesBySpecimenDateD
     const { status, data } = useCovid19AreaData(area);
     const timeFrame = useTimeFrame();
 
+    const datum = useMemo(() => {
+        const date = new Date(BASE_DATE_TIME + (timeFrame * MILLIS_PER_DAY)).toISOString().substring(0, 10);
+        return data.find(d => d.date === date)?.newCasesBySpecimenDate;
+    }, [data, timeFrame]);
+
     const color = useMemo(() => {
         if (status === AreaDataStatus.FETCHING) {
             return '#eef';
         } else if (status === AreaDataStatus.ERROR) {
             return '#fee';
-        } else if (!!!data || timeFrame >= data.length) {
+        } else if (datum === undefined) {
             return '#eee';
-        } else {
-            return `rgb(${Math.max(0, 255 - (data[timeFrame].newCasesBySpecimenDate ^ 3))}, 255, 255)`;
         }
-    }, [status, data, timeFrame])
+        else {
+            return `rgb(255, ${Math.max(0, 255 - (datum ^ 4))}, ${Math.max(0, 255 - (datum ^ 4))})`;
+        }
+    }, [status, datum])
 
-    return (<Node backgroundColor={color} area={area} />)
+return (<Node backgroundColor={color} area={area} datum={datum}/>)
 };
--- src/DataNode/Node.tsx
+++ src/DataNode/Node.tsx
@@ -4,14 +4,16 @@ import { LowerTierLocalAuthorityAreaName } from '../CoronovirusDataGovUkApi/Lowe
 interface NodeProps {
     backgroundColor?: string;
     area?: LowerTierLocalAuthorityAreaName;
+    datum?: number;
 }
 
-export const Node = ({ backgroundColor = 'transparent', area }: NodeProps) => {
+export const Node = ({ backgroundColor = 'transparent', area, datum }: NodeProps) => {
     return (
         <div className='node'>
             <div className='node-top' style={{ borderBottomColor: backgroundColor }} data-testid='node-top' ></div>
             <div className='node-bottom' style={{ borderTopColor: backgroundColor }} data-testid='node-bottom'></div>
-            { (area !== undefined) && (<div className='node-text'>{area}</div>) }
+            { (area !== undefined) && (<div className='node-title'>{area}</div>) }
+            { (datum !== undefined) && (<div className='node-datum'>{datum}</div>)}
         </div>
     );
 }
